ac4a3a33bb055252cee3b7bfda76bd1e3027b28e
rayan <rayanraihani9@gmail.com>
2023-07-16 23:50:14 +0200

    virement de compte

--- index.php
+++ index.php
@@ -3,5 +3,16 @@ require_once "Compte.php";
 require_once "Titulaire.php";
 
 $titu1 = new Titulaire('RAIHANI ','Rayan ',"27-01-2004 ",'Strasbourg');
+$titu2 = new Titulaire('MOHAMMED ',' Foued',"05-11-1997 ",'Strasbourg');
 
+
+$compte1 =new Compte("Livret B",500,"euro",$titu1);
+$compte2 =new Compte("Livret A",20,"euro",$titu2);
+$titu1->DispInfo();
+$titu2->DispInfo();
+
+
+$montantVirement = 300;
+$compte1->virementVersCompte($compte2, $montantVirement);
+$titu2->DispInfo();
 ?>
